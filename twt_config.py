15a9e3ec544a5fbf20cda407fd87614a69bee884
Alaind Sklar <alaindsklar@gmail.com>
2018-03-06 20:56:33 -0800

    added config file

---
+++ twt_config.py
@@ -0,0 +1,4 @@
+consumer_key = "ztUu9ibjrf8zRgYpSE44hkp67"
+consumer_secret = "RwSyNuVCRJsGGvr16GKXzlZdmNIyKa2AfSOT2rhgJyjSA54cM4"
+access_token = "969399393316061184-OPCe6WeILNXVAi0xl42HFjTJsJs4kl8"
+access_token_secret = "s0p2dxdGJVfBiDiWVg7NcG2vRsNo4MkDvdn5MxAP1ng0n"
